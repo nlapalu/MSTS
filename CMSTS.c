185b7fbbd234e4c5f846e208a6b1126fce58606e
Nicolas Lapalu <nicolas.lapalu@inra.fr>
2019-12-20 10:21:55 +0100

    update C code

--- CMSTS.c
+++ CMSTS.c
@@ -50,6 +50,53 @@ static PyObject * phasogram(PyObject *self, PyObject *args)
     return result;
 }
 
+static PyObject * phasogramraw(PyObject *self, PyObject *args)
+{
+    PyObject *chr_values;
+    int window_size;
+    int i;
+    int j;
+    
+    if (!PyArg_ParseTuple(args, "iO", &window_size, &chr_values)){
+	  PyErr_SetString(PyExc_TypeError, "phasogram parameters errors, expected parameters: int, [float]");
+	  return NULL;
+    }
+
+    int nb = PyList_Size(chr_values);
+
+    double *lPhases = malloc(window_size * sizeof(double));
+
+    for (i=0;i<window_size;i++) {
+        lPhases[i] = 0.0;
+    }
+
+    double *lvalues = malloc(nb * sizeof(double));
+
+    for(i=0;i<nb;i++){
+        lvalues[i] = PyFloat_AsDouble(PyList_GetItem(chr_values,i));
+    }
+
+    for (i=0; i<(nb-window_size); i++) {
+        for (j=0; j<window_size; j++){
+           lPhases[j] += lvalues[i]; 
+	}
+    }
+
+    PyObject *lst = PyList_New(window_size);
+    PyObject *num;
+
+    for(i=0;i<window_size;i++){
+        num=PyFloat_FromDouble(lPhases[i]);
+	PyList_SET_ITEM(lst,i,num);
+    }
+
+    PyObject *result= Py_BuildValue("O",lst);
+    Py_DECREF(lst);
+    free(lPhases);
+    free(lvalues);
+    return result;
+}
+
 static PyObject * dinucfrequency(PyObject *self, PyObject *args)
 {
     int distance;
@@ -145,6 +192,7 @@ static char dinucfrequency_doc[] = "dinucfrequency(), doc\n";
 
 static PyMethodDef nucleosome_funcs[] = {
     {"phasogram", (PyCFunction)phasogram, METH_VARARGS, phasogram_doc},
+    {"phasogramraw", (PyCFunction)phasogramraw, METH_VARARGS, phasogram_doc},
     {"dinucfrequency", dinucfrequency, METH_VARARGS, dinucfrequency_doc},
     {NULL, NULL, 0, NULL} /* sentinel */ 
 };	
